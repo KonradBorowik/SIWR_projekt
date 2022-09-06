6161c435c5064892944666892185f31f05fb6b23
Konrad <konrad1borowik@gmail.com>
2022-09-06 22:01:19 +0200

    get middle point of bbox

--- image.py
+++ image.py
@@ -4,6 +4,7 @@ class Image:
     def __init__(self, img_path, bboxes):
         self.img = cv2.imread(img_path)
         self.bboxes = self.get_xyxy(bboxes)
+        self.middle = self.get_mid_point()
 
     def show_image(self):
 
@@ -24,3 +25,9 @@ class Image:
 
                 xyxy.append([x_min, y_min, x_max, y_max])
         return xyxy
+
+    def get_mid_point(self):
+        x_mid = self.bboxes[0] + self.bboxes[2] // 2
+        y_mid = self.bboxes[1] + self.bboxes[3] // 2
+
+        return (x_mid, y_mid)
