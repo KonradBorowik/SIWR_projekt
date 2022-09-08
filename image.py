da3bc36e900900577b57a9d9b51225e1ef539e0c
Konrad <konrad1borowik@gmail.com>
2022-09-08 18:54:48 +0200

    added fields to Image; calcHist method

--- image.py
+++ image.py
@@ -5,9 +5,13 @@ class Image:
         self.img = cv2.imread(img_path)
         self.bboxes = self.get_xyxy(bboxes)
         self.middle = self.get_mid_point()
+        
+        self.x_min = bboxes[0]
+        self.y_min = bboxes[1]
+        self.width = bboxes[2]
+        self.height = bboxes[3]
 
     def show_image(self):
-
         for bbox in self.bboxes:
             x_min, y_min, x_max, y_max = bbox
             cv2.rectangle(self.img, (x_min, y_min), (x_max, y_max), (0, 255, 0), 5)
@@ -31,3 +35,7 @@ class Image:
         y_mid = self.bboxes[1] + self.bboxes[3] // 2
 
         return (x_mid, y_mid)
+
+    def get_bgr_histogram(self):
+        hist_x_min = self.x_min + 0.1 * self.width
+        hist.x_max = self.x_min + 0.9 * self.width
