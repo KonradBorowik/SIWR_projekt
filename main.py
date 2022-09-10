788879909a17539ab1951557dc853638e0d28555
Konrad <konrad1borowik@gmail.com>
2022-09-10 22:51:11 +0200

    f_u matrix for single object

--- main.py
+++ main.py
@@ -19,19 +19,20 @@ def f_b(prev_bbox_count: int, next_bbox_count: int):
         
         matrix.append(row)
 
-    print(f'f_b: {matrix}')
+    # print(f'f_b: {matrix}')
     return matrix
 
 
 def f_u(hists: List, distances: List):
-    matrices = [[-1, 0.55]]
+    matrices = []
     for i_ob, object in enumerate(hists):
-        print(f'object {i_ob}: {object}')
+        # print(f'object {i_ob}: {object}')
+        obj_prob_matrix = [[-1, 0.55]]
         for i_bb, hist in enumerate(object):
-            print(f'hist {i_bb}: {hist}')
-            matrices.append([i_bb, (hist * 5 + distances[i_ob][i_bb] * 10) / (5 + 10)])
-    
-    print(f'f_u: {matrices}')
+            # print(f'hist {i_ob}: {hist}')
+            obj_prob_matrix.append([i_bb, (hist * 0.5 + distances[i_ob][i_bb] * 0.1) / (0.5 + 0.1)])
+        matrices.append(obj_prob_matrix)
+    # print(f'f_u: {matrices}')
     return matrices
 
 
@@ -43,16 +44,16 @@ if __name__ == '__main__':
         if i < len(images) -1:
             print(f'pair {i}')
             if image.bbox_count == 0 or images[i+1].bbox_count == 0:
-                print("empty")
+                print()
                 continue
 
             hists = compare_histograms(image, images[i+1])
             distances = calc_dist(image, images[i+1])
 
-
             matrix_f_b = f_b(image.bbox_count, images[i+1].bbox_count)
-
+            print(f'f_b: {matrix_f_b}')
             matrices_f_u = f_u(hists, distances)
+            print(f'f_u: {matrices_f_u}')
             
             if i == 5:
                 break
