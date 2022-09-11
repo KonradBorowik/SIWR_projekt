37e76b857dcb6ae448c941361688d5be3b968392
Konrad <konrad1borowik@gmail.com>
2022-09-11 13:00:08 +0200

    changed direction of comparison

--- histograms.py
+++ histograms.py
@@ -5,15 +5,15 @@ from image import Image
 from visualizer import get_images
 
 
-def compare_histograms(prev: Image, next: Image) -> List:
+def compare_histograms(prev: Image, current: Image) -> List:
     prev_hists = prev.get_upper_histograms()
-    next_hists = next.get_upper_histograms()
+    current_hists = current.get_upper_histograms()
 
     hists_similarity = []
-    for prev_hist in prev_hists:
+    for current_hist in current_hists:
         hist_similarity = []
-        for next_hist in next_hists:
-            hist_similarity.append(cv2.compareHist(prev_hist, next_hist, cv2.HISTCMP_CORREL))
+        for prev_hist in prev_hists:
+            hist_similarity.append(cv2.compareHist(current_hist, prev_hist, cv2.HISTCMP_CORREL))
         hists_similarity.append(hist_similarity)
     
     return hists_similarity
