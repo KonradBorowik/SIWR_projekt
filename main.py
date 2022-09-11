519a74299427742fcc92690f63a79f3275d99b2b
Konrad <konrad1borowik@gmail.com>
2022-09-11 14:39:56 +0200

    discrete factor for single object

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from image import Image
 from visualizer import get_images
 from histograms import compare_histograms
 from distance import calc_dist
+from pgmpy.models import FactorGraph
+from pgmpy.factors.discrete import DiscreteFactor
 
 
-def f_b(prev_bbox_count: int, next_bbox_count: int):
+def f_b(prev_bbox_count: int, current_bbox_count: int):
     matrix = []
-    for ver in range(prev_bbox_count+1):
+    for ver in range(current_bbox_count+1):
         row = []
-        for hor in range(next_bbox_count+1):
+        for hor in range(current_bbox_count+1):
             if ver == hor != 0:
                 row.append(0)
             else:
@@ -25,15 +27,33 @@ def f_b(prev_bbox_count: int, next_bbox_count: int):
 
 def f_u(hists: List, distances: List):
     matrices = []
-    for i_ob, object in enumerate(hists):
+    nodes = []
+    print(f'bboxes count = {len(hists)}')
+    for i_ob, object in enumerate(hists): #object_count
         # print(f'object {i_ob}: {object}')
-        obj_prob_matrix = [[-1, 0.55]]
-        for i_bb, hist in enumerate(object):
+        nodes.append(f'bbox_{i_ob}')
+        obj_prob_matrix = [0.55]
+        for i_bb, hist in enumerate(object): #connection options
             # print(f'hist {i_ob}: {hist}')
-            obj_prob_matrix.append([i_bb, (hist * 0.5 + distances[i_ob][i_bb] * 0.1) / (0.5 + 0.1)])
+            obj_prob_matrix.append((hist * 0.5 + distances[i_ob][i_bb] * 0.1) / (0.5 + 0.1))
         matrices.append(obj_prob_matrix)
     # print(f'f_u: {matrices}')
-    return matrices
+    return matrices, nodes
+
+
+def create_graph(f_b, f_u, nodes):
+    Graph = FactorGraph()
+    Graph.add_nodes_from(nodes)
+
+    edges = []
+    dfs = []
+    print(len(nodes))
+    print(len(f_u))
+    print(f_u)
+    for i, node in enumerate(nodes):
+        df = DiscreteFactor([node], [len(f_u[i])], f_u[i])
+
+    Graph.add_factors(df)
 
 
 if __name__ == '__main__':
@@ -41,19 +61,23 @@ if __name__ == '__main__':
     for i, image in enumerate(images):
         hists = []
         distances = []
-        if i < len(images) -1:
+        if i > 0:
             print(f'pair {i}')
-            if image.bbox_count == 0 or images[i+1].bbox_count == 0:
+            if images[i-1].bbox_count == 0 or image.bbox_count == 0:
                 print()
                 continue
-
-            hists = compare_histograms(image, images[i+1])
-            distances = calc_dist(image, images[i+1])
-
-            matrix_f_b = f_b(image.bbox_count, images[i+1].bbox_count)
+            print(f'prev bbox count = {images[i-1].bbox_count}')
+            print(f'curr bbox count = {image.bbox_count}')
+            hists = compare_histograms(images[i-1], image)
+            distances = calc_dist(images[i-1], image)
+            print(hists)
+            matrix_f_b = f_b(images[i-1].bbox_count, image.bbox_count)
             print(f'f_b: {matrix_f_b}')
-            matrices_f_u = f_u(hists, distances)
+            matrices_f_u, nodes_names = f_u(hists, distances)
             print(f'f_u: {matrices_f_u}')
-            
-            if i == 5:
+            print(nodes_names)
+
+            create_graph(matrix_f_b, matrices_f_u, nodes_names)
+            print('----')
+            if i == 3:
                 break
